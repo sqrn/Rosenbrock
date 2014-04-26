ab5ea071f310bd5b9abad9db2f944f3b58b0c01c
Mariusz Skóra <m.skora@gmail.com>
2014-04-26 13:29:02 +0200

    first upload
    First upload

---
+++ Genotype.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosenbrock
+{
+    class Genotype
+    {
+        public int Length {get; set;}
+        public EvolutionStrategy evolutionStrategy{get; set;}
+        public List<double> Genes { get; set; }
+
+        public Genotype(int length, EvolutionStrategy algorithm)
+        {
+            evolutionStrategy = algorithm;
+            Length = length;
+
+            Genes = GenerateGenes();
+        }
+
+        private List<double> GenerateGenes()
+        {
+            List<double> doubleList = new List<double>();
+
+            for(int i = 0; i < Length; i++)
+            {
+                //var tmpDoubleArray = new double[4];
+                //evolutionStrategy.random.NextDouble();
+                //doubleList.AddRange(tmpDoubleArray);
+                doubleList.Add(evolutionStrategy.random.NextDouble());
+            }
+            return doubleList;
+        }
+
+        public void ShowGenes()
+        {
+            foreach (var val in Genes)
+            {
+                Console.WriteLine(val);
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosenbrock
+{
+    public delegate double FunctionToOptimize(double[] values);
+
+    class Program
+    {
+        /// <summary>
+        /// Badana funkcja - Rosenbrock
+        /// sum(100*(x(i)−x(i−1)^2)^2   + ( 1−x(i−1) )^2 )
+        /// </summary>
+        public static double Function(double[] values)
+        {
+            List<double> valueList = new List<double>();
+            double value;
+
+            for (int i = 1; i < values.Length - 1; i++)
+            {
+                value = 100 * Math.Pow(values[i] - Math.Pow(values[i - 1], 2), 2) + Math.Pow(1 - values[i - 1], 2);
+                valueList.Add(value);
+            }
+            double f1 = valueList.Sum();
+            //Console.WriteLine("Wynik funkcji to: " + f1);
+            return f1;
+        }
+
+
+        static void Main(string[] args)
+        {
+            //double[] a = {1.2, 2.1, 3.5};
+            //Function(a);
+
+            int populationSize = 100;
+
+            EvolutionStrategy strategy = new EvolutionStrategy();
+            Genotype genotype = new Genotype(populationSize, strategy);
+
+            genotype.ShowGenes();
+
+            var function = new FunctionToOptimize(Function);
+
+            Console.ReadKey();
+        }
+    }
+}
